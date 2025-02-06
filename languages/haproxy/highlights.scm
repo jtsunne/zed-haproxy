a422ac3eab8534bd20df6c5f1abd1847e70800ff
jtsunne <jtsunne@protonmail.com>
2025-02-05 21:11:40 -0600

    first version

--- languages/haproxy/highlights.scm
+++ languages/haproxy/highlights.scm
@@ -1,4 +1,42 @@
+; Подсветка комментариев
 (comment) @comment
 
-((identifier) @keyword
-  (#match? @keyword "^(global|daemon|log|maxconn|retries|timeout|listen|bind|mode|stats|acl|use_backend)$"))
+(global_defaults) @keyword
+(fbl_set) @keyword
+
+((identifier) @function.method
+    (#match? @function.method "^(set-header|add-header)$"))
+
+(identifier) @attribute
+
+(section_name) @string
+
+((word) @keyword
+    (#match? @keyword "^(enable|disable|ssl|crt)$"))
+
+((word) @constant
+    (#match? @constant "^(if|acl|use_backend)$"))
+
+((word) @constant
+    (#match? @constant "^(http|https|ssl_fc|X-Forwarded-Port|X-Forwarded-Proto|forwardfor)$"))
+
+((word) @number
+    (#match? @number "^(\\d+)(.)?$"))
+
+((word) @function
+    (#match? @function "^(\.+)\/(\[^\/\]+)$"))
+
+; ((word) @variable
+;     (#match? @variable "^(\\d+)|(\\*:(\\d+))$"))
+
+((word) @function
+    (#match? @function "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$"))
+
+((word) @function
+    (#match? @function "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}:\\d{1,5}$"))
+
+((word) @function
+    (#match? @function "^\\*:\\d{1,5}$"))
+
+((word) @function
+    (#match? @function "^(set-header|add-header|hdr)$"))
